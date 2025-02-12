1a3b6022f5c7a2fbdeddf88c49d3e94b964d8117
pedro-rothen <p.rothen@outlook.cl>
2025-02-11 22:10:12 -0300

    Filter cities

--- app/(tabs)/cities.tsx
+++ app/(tabs)/cities.tsx
@@ -1,14 +1,16 @@
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, TextInput } from 'react-native';
 import { View } from '@/components/Themed';
 import CityCard from '@/components/cityCard';
 import { Link } from 'expo-router';
 import { useEffect, useState } from "react";
 import { ActivityIndicator } from "react-native";
 import { fetchCities } from '@/services/cities';
+import { SearchBar } from 'react-native-screens';
 
 export default function Cities() {
   const [cities, setCities] = useState<City[] | null>(null);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const getCities = async () => {
@@ -32,8 +34,13 @@ export default function Cities() {
   if (cities !== null) {
     return (
       <View>
+        <TextInput
+          placeholder="Buscar"
+          value={search}
+          onChangeText={setSearch}
+        />
         <FlatList 
-        data={cities} 
+        data={cities.filter((city) => city.name.toLowerCase().includes(search.toLowerCase()))} 
         renderItem={({ item }) => (
           <Link href={`/${item.name}`}>
               <CityCard {...item} />
